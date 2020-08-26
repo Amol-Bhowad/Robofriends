45af0a31feaf19676758847ffa020ad33b2df927
Amol Bhowad <39921906+Amol-Bhowad@users.noreply.github.com>
2020-08-26 10:25:45 +0530

    First Commit

---
+++ src/robots.js
@@ -0,0 +1,63 @@
+export const robots = [
+{
+	id: 1,
+	name: 'Amol',
+	username: 'amolbhowad',
+	email:'amolbhowad@gmail.com'
+},
+{
+	id: 2,
+	name: 'Ann',
+	username: 'anna',
+	email:'annabelle@gmail.com'
+},
+{
+	id: 3,
+	name: 'Subhash',
+	username: 'subashh',
+	email:'subashh@gmail.com'
+},
+{
+	id: 4,
+	name: 'Alkayda',
+	username: 'alkayda',
+	email:'alkayda@gmail.com'
+},
+{
+	id: 5,
+	name: 'Dats',
+	username: 'dats01',
+	email:'dats@gmail.com'
+},
+{
+	id: 6,
+	name: 'smith',
+	username: 'stevesmith',
+	email:'stevesmith@gmail.com'
+},
+{
+	id: 7,
+	name: 'amanda',
+	username: 'amandas',
+	email:'amandas@gmail.com'
+},
+{
+	id: 8,
+	name: 'Ds',
+	username: 'dmands',
+	email:'dmands@gmail.com'
+},
+{
+	id: 9,
+	name: 'Golu',
+	username: 'golukolu',
+	email:'golukolu@gmail.com'
+},
+{
+	id: 10,
+	name: 'Dats',
+	username: 'dats01',
+	email:'dats@gmail.com'
+},
+
+]
